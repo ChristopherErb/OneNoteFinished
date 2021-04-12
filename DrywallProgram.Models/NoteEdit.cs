28acfc97b95ee1877769946c4e2fb4cb9d1aa599
ChristopherErb01 <christophererb01@gmail.com>
2021-04-11 22:26:44 -0400

    Delete

---
+++ DrywallProgram.Models/NoteEdit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrywallProgram.Models
+{
+   public class NoteEdit
+    {
+
+        public int NoteId { get; set; }
+        public String Title { get; set; }
+        public String Content { get; set; }
+
+
+    }
+}
